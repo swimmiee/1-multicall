8c7dcf15c2218ed6cfe7687024bf8a0524c5b36d
Sooyoung Lee <dannylisa67@gmail.com>
2023-07-03 20:49:55 +0900

    readable balances

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,15 @@
 import { chainDataList } from "data/chains";
 import { TokenDataList } from "data/tokens";
 import { getTokenBalances } from "getTokenBalances";
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 
 function App() {
-  const userAddress = "0x49b68E394F715871B48130Abaa64278532E54eF2";
+  const userAddress = "0xAA289325d1afc4AA040281b10dD9f10A8560D296";
 
   useEffect(() => {
     const chain = chainDataList[0];
-    getTokenBalances(
-      chain,
-      TokenDataList[chain.id].map(t => t.address),
-      userAddress
-    )
-  },[])
-
-
+    getTokenBalances(chain, TokenDataList[chain.id], userAddress)
+  }, []);
 
   return (
     <div className="px-8 py-2 flex flex-col gap-6 divide-y [&>div]:pt-4">
@@ -24,7 +18,6 @@ function App() {
         <h2 className="font-bold text-2xl mb-2">1. Connect Wallet</h2>
         <button className="btn">Connect wallet</button>
       </div>
-      
     </div>
   );
 }
--- src/getTokenBalances.ts
+++ src/getTokenBalances.ts
@@ -1,17 +1,19 @@
-import { Contract, ethers, providers } from "ethers";
-import { ChainData } from "interfaces/data/chain-data.interface";
+import { Contract, constants, ethers, providers } from "ethers";
 import { Multicall2 } from "typechain";
 import { ERC20Interface } from "typechain/ERC20";
-import { Interface } from "ethers/lib/utils";
+import { Interface, formatUnits } from "ethers/lib/utils";
+import { Multicall2Interface } from "typechain/Multicall2";
+import { ChainData } from "interfaces/data/chain-data.interface";
+import { TokenData } from "interfaces/data/token-data.interface";
 import MULTICALL2_ABI from "abi/Multicall2.json";
 import ERC20_ABI from "abi/ERC20.json";
 
 // 여러 토큰의 잔액을 조회하는 함수를 작성합니다.
 export const getTokenBalances = async (
   chain: ChainData,
-  tokenAddresses: string[],
+  tokens: TokenData[],
   owner: string
-) => {
+): Promise<string[]> => {
   // 1. provider를 선언합니다
   // TIP
   const provider = new providers.FallbackProvider(
@@ -30,16 +32,26 @@ export const getTokenBalances = async (
   // 여러 토큰에 대해서 owner의 balance 구하기
   // ERC20의 balanceOf 함수를 호출하는 callData를 만들어야 합니다.
   const erc20Itf = new Interface(ERC20_ABI) as ERC20Interface;
+  const multicallItf = new Interface(MULTICALL2_ABI) as Multicall2Interface;
+  const { returnData } = await multicallContract.aggregate(
+    tokens.map((token) =>
+      token.address === constants.AddressZero // native token인 경우
+        ? {
+            target: chain.multicallAddress,
+            callData: multicallItf.encodeFunctionData("getEthBalance", [owner]),
+          }
+        : {
+            target: token.address,
+            callData: erc20Itf.encodeFunctionData("balanceOf", [owner]),
+          }
+    )
+  );
 
-  const result = await multicallContract.aggregate([
-    {
-        target: tokenAddresses[0],
-        callData: erc20Itf.encodeFunctionData("balanceOf", [owner]),
-    }
-    // { target: "0x1", callData: "balanceOf()" },
-    // { target: "0x2", callData: "balanceOf()" },
-    // { target: "0x3", callData: "balanceOf()" },
-  ]);
+  console.log(
+    returnData.map((balance, i) => formatUnits(balance, tokens[i].decimals))
+  );
 
-  console.log(result)
+  return returnData.map((balance, i) =>
+    formatUnits(balance, tokens[i].decimals)
+  );
 };
